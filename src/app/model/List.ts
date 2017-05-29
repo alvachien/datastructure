aa00920f573c896a9a40e1dc6d3a04f21dd5d86d
Alva Chien <alvachien@live.com>
2017-05-29 18:08:49 +0800

    Add List

---
+++ src/app/model/List.ts
@@ -0,0 +1,34 @@
+
+export class List<T> {
+    constructor() {        
+    }
+
+    public InitList() {        
+    }
+
+    public ListEmpty(): boolean {
+        return false;
+    }
+
+    public ClearList() {
+
+    }
+
+    public GetElem(index: number) {
+
+    }
+
+    public LocateElem(elem) {
+
+    }
+
+    public Insert(elem) {
+
+    }
+    public Delete(index) {
+
+    }
+    public Length() {
+        
+    }
+}
