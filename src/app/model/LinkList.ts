3382c59fa74e63fe85a9f108e1b46612cf84fcd7
Alva Chien <alvachien@163.com>
2017-06-02 18:08:54 +0800

    Add new codes

--- src/app/model/LinkList.ts
+++ src/app/model/LinkList.ts
@@ -5,6 +5,10 @@ export class LinkListNode<T> {
     private _data: T;
     private _next:LinkListNode<T> = null;
 
+    constructore() {
+        this._next = null;
+    }
+
     get Data(): T {
         return this._data;
     }
@@ -53,6 +57,34 @@ export class LinkList<T> implements IList<T> {
     }
 
     public InsertElement(index: number, elem: T): boolean {
-        
-    }    
+        if (index < 0 || index > this._length) return false;
+        if (elem === null) return false;
+
+        let cur: LinkListNode<T> = this._head;
+        for(let i = 0; i < index; i++) {
+
+        }
+        while(cur !== null) {
+
+        }
+    }   
+
+    public AppendElement(elem: T) : number {
+        let cur: LinkListNode<T> = this._head;
+        while(cur !== null) {
+            cur = cur.Next;
+        }
+
+        let newnode = new LinkListNode<T>();
+        newnode.Data = elem;
+        newnode.Next = null;
+
+        cur.Next = newnode;
+
+        return ++this._length;
+    } 
+
+    public DeleteElement(index: number): boolean {
+        return false;
+    }
 }
