998429164043da9e10f53e3567707790e8bb59d7
Alva Chien <alvachien@163.com>
2017-07-14 18:32:01 +0800

    Adding sorting algorithm

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # INTRODUCTION
-Data Structure, including List, Tree, Graph, and Sorting Algorithm. 
+This project is all about building a lib for Data Structures and algorithms. 
 
-Written by TypeScript.
+The Data Structure part, including List, Tree, Graph, and others. 
 
-Based on the books
-- <Introduction to Algorithms>, Third Edition, by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein
-- <数据结构 用面向对象方法与C++描述>， Tsinghua University Press
+The Algorithm part, including sorting, searching and others. 
+
+The whole project written with TypeScript and scaffold with Angular-CLI for demo-app.
+
+Based on the books:
+- Introduction to Algorithms, Third Edition, by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein
+- 数据结构 用面向对象方法与C++描述， Tsinghua University Press
 - Others
 
 # CONTENT
@@ -15,7 +19,7 @@ Folder lib (src\lib) contains two sub folders, and it has been exported via inde
 
 Folder app (src\app) contains an example application which used to demonstrate the demos, it is a web app using Angular Material.
 
-## HOW TO RUN
+## RUN DEMO APP
 Download or clone this repository, and then run the following commands in sequence:
 - npm install
 - ng serve
@@ -48,8 +52,13 @@ TBD.
 
 ## ALGORITHM
 - The algorithm **KMP** which offer the functionality to search source string from the target string.
-
-TBD.
+- The algorithm **InsertionSort** using the insertion sort upon the array.
+- The algorithm **BinaryInsertSort** based on **InsertionSort** but improves the way to search for suitable position to insert.
+- The algorithm **BubbleSort** using the bubble-sort on the array: pickup the biggest and move it to the tail of the array.
+- The algorithm **QuickSort** based on **BubbleSort** but use recursive way to handle two parts of the array.
+- The algorithm **SelectionSort** choose the min (or max) from the unsorted part and append it to the sorted part.
+- The algorithm **CountingSort** give an item a n-th position because there are n-1 item less (or bigger) than it. This algorithm suits only for number based array.
+- TBD 
 
 # AUTHOR
 **Alva Chien | 钱红俊**
--- src/lib/model/Algorithm.ts
+++ src/lib/model/Algorithm.ts
@@ -9,22 +9,23 @@
  *  2.1 Insert sort
  *  2.2 Bubble sort
  *  2.3 Quick sort
- *  2.4 
+ *  2.4 Selection sort
+ *  2.5 Counting sort
  * 3. Others
  * 
  */
 
-function KMP_MakeNext(arr: string) : number[] {
-    let q : number  = 0;
+function KMP_MakeNext(arr: string): number[] {
+    let q: number = 0;
     let k: number = 0;
     let len: number = 0;
     len = arr.length;
     let next: number[] = [];
 
     next.push(0);
-    for(q = 1, k = 0; q < len; ++q) {
-        while(k > 0 && arr.charAt(q) !== arr.charAt(k)) {
-            k = next[k-1];
+    for (q = 1, k = 0; q < len; ++q) {
+        while (k > 0 && arr.charAt(q) !== arr.charAt(k)) {
+            k = next[k - 1];
         }
 
         if (arr.charAt(q) === arr.charAt(k)) {
@@ -47,13 +48,13 @@ export function KMP(arr1: string, arr2: string): number | null {
     let m: number = arr2.length;
     let next: number[] = KMP_MakeNext(arr2);
 
-    for(let i = 0, q = 0; i < n; ++i) {
+    for (let i = 0, q = 0; i < n; ++i) {
         while (q > 0 && arr2.charAt(q) !== arr1.charAt(i)) {
             q = next[q - 1];
         }
 
         if (arr2.charAt(q) === arr1.charAt(i)) {
-            q ++;
+            q++;
         }
 
         if (q === m) {
@@ -67,6 +68,7 @@ export function KMP(arr1: string, arr2: string): number | null {
 /**
  * Insertion sort: an in-place sorting
  * @param datalist: data list for sorting
+ * 
  * Loop through the list:
  *  When inserting the i-th element, the previous list (0 .. i-1) has been sorted already.
  *  And, then insert i-th element into the (0 .. i-1), and shift the positions for impacted elements.
@@ -76,7 +78,7 @@ export function InsertionSort<T>(datalist: T[]): boolean {
         return false;
     }
 
-    for(let i = 1; i < datalist.length; i++) {
+    for (let i = 1; i < datalist.length; i++) {
         InsertionSort_Insert(datalist, i);
     }
     return true;
@@ -86,9 +88,9 @@ function InsertionSort_Insert<T>(datalist: T[], idx: number) {
     let tmp: T = datalist[idx];
     let j: number = idx;
 
-    while(j > 0 && tmp < datalist[j - 1]) {
+    while (j > 0 && tmp < datalist[j - 1]) {
         datalist[j] = datalist[j - 1];
-        j --;
+        j--;
     }
 
     datalist[j] = tmp;
@@ -97,6 +99,7 @@ function InsertionSort_Insert<T>(datalist: T[], idx: number) {
 /**
  * Binary insertion sort: an in-place sorting by using binary searching
  * @param datalist: data list for sorting
+ * 
  * Loop through the list:
  *  When inserting the i-th element, the previous list (0 .. i-1) has been sorted already.
  *  And, then insert i-th element into the (0 .. i-1), and shift the positions for impacted elements.
@@ -106,7 +109,7 @@ export function BinaryInsertSort<T>(datalist: T[]): boolean {
         return false;
     }
 
-    for(let i = 1; i < datalist.length; i++) {
+    for (let i = 1; i < datalist.length; i++) {
         BinaryInsertSort_Insert(datalist, i);
     }
 
@@ -118,7 +121,7 @@ function BinaryInsertSort_Insert<T>(datalist: T[], idx: number) {
     let left: number = 0;
     let right: number = idx - 1;
 
-    while(left <= right) {
+    while (left <= right) {
         let middle: number = Math.floor((left + right) / 2);
         if (tmp < datalist[middle]) {
             right = middle - 1;
@@ -127,8 +130,8 @@ function BinaryInsertSort_Insert<T>(datalist: T[], idx: number) {
         }
     }
 
-    for(let k = idx - 1; k >= left; k --) {
-        datalist[k + 1] = datalist[k];            
+    for (let k = idx - 1; k >= left; k--) {
+        datalist[k + 1] = datalist[k];
     }
 
     datalist[left] = tmp;
@@ -137,14 +140,122 @@ function BinaryInsertSort_Insert<T>(datalist: T[], idx: number) {
 /**
  * Bubble sort: an in-place exchange sort
  * @param datalist: data list for sorting
- * Loop through the list:
- *  When inserting the i-th element, the previous list (0 .. i-1) has been sorted already.
- *  And, then insert i-th element into the (0 .. i-1), and shift the positions for impacted elements.
+ * 
+ * Two nested loops through the list:
+ * First loop through 0 .. length - 1
+ *  Second loop through available elements (length - 1 - i)
+ *  Compare the two elements, exchange the larger one to higher position.
  */
 export function BubbleSort<T>(datalist: T[]): boolean {
     if (datalist.length <= 0) {
         return false;
     }
 
+    for(let i = 0; i < datalist.length - 1; i ++) {
+        for(let j = 0; j < datalist.length - 1 - i; j ++) {
+            if (datalist[j] > datalist[j + 1]) {
+                let tmp = datalist[j];
+                datalist[j] = datalist[j + 1];
+                datalist[j + 1] = tmp;
+            }
+        }
+    }
+    
     return true;
+}
+
+/**
+ * Quick sort: an in-place exchange sort
+ * @param datalist: data list for sorting
+ * 
+ * The core method of quick sort is the recursive.
+ * Choose one number, split the whole array to two arrays: one all smaller than that number, the other all larger than it.
+ * Then recursively process the two new arrays with the same idea.
+ */
+export function QuickSort<T>(datalist: T[]): boolean {
+    QuickSortImpl(datalist, 0, datalist.length - 1);
+    return true;
+}
+
+function QuickSortImpl<T>(datalist: T, left: number, right: number) {
+    if (left >= right) {
+        return;
+    }
+
+    let i: number = left;
+    let j: number = right;
+    let key: T = datalist[left];
+
+    while (i < j) {
+        while(i < j && key <= datalist[j]) {
+            j --;
+        }
+
+        datalist[i] = datalist[j];
+
+        while(i < j && key >= datalist[i]) {
+            i ++;
+        }
+        datalist[j] = datalist[i];
+    }
+
+    datalist[i] = key;
+    QuickSortImpl(datalist, left, i - 1);
+    QuickSortImpl(datalist, i + 1, right);
+}
+
+/**
+ * Selection sort: an in-place sorting
+ * @param datalist data list for sorting
+ * 
+ * The idea for selection sort is, choose the min (or max) from un-sorted part, and append it to the sorted part
+ */
+export function SelectionSort<T>(datalist: T[]) {
+    let i: number = 0;
+    for (i = 0; i < datalist.length - 1; i ++) {
+        let min:number = i;
+        for(let j: number = i + 1; j < datalist.length; j++) {
+            if (datalist[min] > datalist[j]) {
+                min = j;
+            }
+        }
+
+        if (min !== i) {
+            let tmp: T = datalist[min];
+            datalist[min] = datalist[i];
+            datalist[i] = tmp;
+        }
+    }
+}
+
+/**
+ * Counting sort
+ * @param datalist: data list for sorting
+ * @param max: maximum  
+ * 
+ * The idea behind is, build an array for all possible values, the element of the list are map to that huge array with correct position.
+ */
+export function CountingSort(datalist: number[], max: number) {
+    let tmp: number[] = [];
+    let out: number[] = [];
+
+    for(let i = 0; i < max; i ++) {
+        tmp.push(0);
+    }
+    for(let i = 0; i < datalist.length; i ++) {
+        tmp[datalist[i]] += 1;
+        out.push(0);
+    }
+
+    for(let i = 1; i < max; i ++) {
+        tmp[i] = tmp[i - 1] + tmp[i];
+    }
+    for(let i = datalist.length - 1; i >= 0; i --) {
+        out[tmp[datalist[i]] - 1] = datalist[i];
+        tmp[datalist[i]] --;
+    }
+
+    for(let i = 0; i < datalist.length; i ++) {
+        datalist[i] = out[i];
+    }
 }
