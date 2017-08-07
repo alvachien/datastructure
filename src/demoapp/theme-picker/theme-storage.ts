be7ea802b96658bc740baa18d8eaf33506eda4e2
Alva Chien <alvachien@163.com>
2017-08-07 19:04:00 +0800

    New changes for dynamic themes

---
+++ src/demoapp/theme-picker/theme-storage.ts
@@ -0,0 +1,30 @@
+import {Injectable, EventEmitter} from '@angular/core';
+
+export interface DocsSiteTheme {
+  href: string;
+  accent: string;
+  primary: string;
+  isDark?: boolean;
+  isDefault?: boolean;
+};
+
+
+@Injectable()
+export class ThemeStorage {
+  static storageKey = 'demoapp-theme-storage-current';
+
+  public onThemeUpdate: EventEmitter<DocsSiteTheme> = new EventEmitter<DocsSiteTheme>();
+
+  public storeTheme(theme: DocsSiteTheme) {
+    window.localStorage[ThemeStorage.storageKey] = JSON.stringify(theme);
+    this.onThemeUpdate.emit(theme);
+  }
+
+  public getStoredTheme(): DocsSiteTheme {
+    return JSON.parse(window.localStorage[ThemeStorage.storageKey] || null);
+  }
+
+  public clearStorage() {
+    window.localStorage.removeItem(ThemeStorage.storageKey);
+  }
+}
