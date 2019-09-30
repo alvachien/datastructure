21668f5273ef5806e61d74df50ecffab5e5382b2
Alva Chien <alvachien@163.com>
2019-09-30 18:03:22 +0800

    Add new codes

--- src/lib/utility/UIUtility.ts
+++ src/lib/utility/UIUtility.ts
@@ -10,17 +10,15 @@
  */
 
 export class UIUtility {
-  public static getDocumentHead(doc: any): any {
-    if (!doc) {
-      doc = document;
-    }
+  public static getDocumentHead(doc?: any): any {
+    let docinuse = doc || document;
 
-    return doc.head || doc.getElementsByTagName('head')[0] || doc.documentElement;
+    return docinuse.head || docinuse.getElementsByTagName('head')[0] || docinuse.documentElement;
   }
-  public static createElementfunction(tag: string, ns: string = 'http://www.w3.org/1999/xhtml'): Element | HTMLElement {
+  public static createElement(tag: string, ns: string = 'http://www.w3.org/1999/xhtml'): Element | HTMLElement {
     return document.createElementNS ?
-           document.createElementNS(ns, tag) :
-           document.createElement(tag);
+      document.createElementNS(ns, tag) :
+      document.createElement(tag);
   }
   public static hasCssClass(element: HTMLElement, cssClassName: string): boolean {
     const classes: string[] = (element.className + '').split(/\s+/g);
@@ -67,18 +65,48 @@ export class UIUtility {
       UIUtility.removeCssClass(element, cssClassName);
     }
   }
-  public static hasCssString(id, doc): boolean {
+  public static hasCssString(id: string, doc?: HTMLDocument): boolean {
     let index = 0, sheets;
-    doc = doc || document;
+    let docinuse: HTMLDocument = doc || document;
+    let existingStyles = docinuse.getElementsByTagName('style');
 
-    if (doc.createStyleSheet && (sheets = doc.styleSheets)) {
-        while (index < sheets.length)
-            if (sheets[index++].owningElement.id === id) return true;
-    } else if ((sheets = doc.getElementsByTagName("style"))) {
-        while (index < sheets.length)
-            if (sheets[index++].id === id) return true;
+    while (index < existingStyles.length) {
+      if (existingStyles[index++].id === id) {
+        return true;
+      }
     }
 
     return false;
+  }
+  // public static importCssString(cssText: string, id: string, doc?: HTMLDocument): void {
+  //   let docinuse: HTMLDocument = doc || document;
+  //   if (id && UIUtility.hasCssString(id, docinuse))
+  //     return;
+
+  //   var style;
+
+  //   if (id)
+  //       cssText += "\n/*# sourceURL=ace/css/" + id + " */";
+
+  //   if (doc.createStyleSheet) {
+  //       style = doc.createStyleSheet();
+  //       style.cssText = cssText;
+  //       if (id)
+  //           style.owningElement.id = id;
+  //   } else {
+  //       style = exports.createElement("style");
+  //       style.appendChild(doc.createTextNode(cssText));
+  //       if (id)
+  //           style.id = id;
+
+  //       exports.getDocumentHead(doc).appendChild(style);
+  //   }
+  // }
+  public static importCssStylsheet(uri: string) {
+    var link = UIUtility.createElement('link') as HTMLLinkElement;
+    link.rel = 'stylesheet';
+    link.href = uri;
+
+    UIUtility.getDocumentHead().appendChild(link);
   }
 }
